89fe6acadfe1e6129118742363d9f54e209ea03f
filipaivars <ivars.filipa@gmail.com>
2015-10-11 14:01:14 +0100

    caracteres especiais

--- Dominup.pl
+++ Dominup.pl
@@ -35,7 +35,7 @@ piece(34,7,5).
 piece(35,7,6).
 piece(36,7,7).
 
-/*cada celula contem list[id.piece] valor list[orientaçao] */
+/*cada celula contem list[id.piece] valor list[orientacao] */
 board( [
     [ [ [], -1 , [] ], [ [], -1 , [] ], [ [], -1 , [] ] ],
     [ [ [], -1 , [] ], [ [3], 4 , [1] ], [ [], -1 , [] ] ],
@@ -46,7 +46,7 @@ board( [
 p1([]). 
 p2([]).
 
-/*contem o board,peças do p1 e p2, e o proximo jogador a jogar)*/
+/*contem o board,pecas do p1 e p2, e o proximo jogador a jogar)*/
 
 
 game(Board-P1-P2-Nextplayer).
@@ -58,149 +58,148 @@ barra :- put_code(124).
 
 printlinenumber(0,_).
 printlinenumber(X,Y) :- 
-	write('  '),write(Y),write('   '),
-	X1 is X - 1,
-	Y1 is Y + 1,
-	printlinenumber(X1,Y1).
-	
+        write('  '),write(Y),write('   '),
+        X1 is X - 1,
+        Y1 is Y + 1,
+        printlinenumber(X1,Y1).
+        
 printboardline(0).
 printboardline(X) :-
-	X > 0,
-	write('-----|'),
-	X1 is X - 1,
-	printboardline(X1).
+        X > 0,
+        write('-----|'),
+        X1 is X - 1,
+        printboardline(X1).
 
 printblankcell :- write('     |').
 
 printcell1([]).
 printcell1([[],_,_]) :- 
-	printblankcell.
+        printblankcell.
 printcell1([[_H1|_T1],_V,[H2|_T2]]) :- 
-	H2 =:= 1,
-	barra,upperscore,upperscore,upperscore,upperscore,barra.
+        H2 =:= 1,
+        barra,upperscore,upperscore,upperscore,upperscore,barra.
 printcell1([[_H1|_T1],_V,[H2|_T2]]) :- 
-	H2 =:= 2,
-	barra,upperscore,upperscore,upperscore,barra,barra.
+        H2 =:= 2,
+        barra,upperscore,upperscore,upperscore,barra,barra.
 printcell1([[_H1|_T1],_V,[H2|_T2]]) :- 
-	H2 =:= 3,
-	upperscore,upperscore,upperscore,upperscore,barra,barra.
+        H2 =:= 3,
+        upperscore,upperscore,upperscore,upperscore,barra,barra.
 printcell1([[_H1|_T1],_V,[H2|_T2]]) :- 
-	H2 =:= 4,
-	barra,space,space,space,barra,barra.
+        H2 =:= 4,
+        barra,space,space,space,barra,barra.
 
 printcell2([]).
 printcell2([[],_,_]) :- 
-	printblankcell.
+        printblankcell.
 printcell2([[H1|T1],V,[H2|_T2]]) :- 
-	H2 =:= 1,
-	length([H1|T1],X),
-	barra,write(V),put_char('-'),write(X),space,barra.
+        H2 =:= 1,
+        length([H1|T1],X),
+        barra,write(V),put_char('-'),write(X),space,barra.
 printcell2([[H1|T1],V,[H2|_T2]]) :- 
-	H2 =:= 2,
-	length([H1|T1],X),
-	barra,write(V),put_char('-'),write(X),barra,barra.
+        H2 =:= 2,
+        length([H1|T1],X),
+        barra,write(V),put_char('-'),write(X),barra,barra.
 printcell2([[H1|T1],V,[H2|_T2]]) :- 
-	H2 =:= 3,
-	length([H1|T1],X),
-	space,write(V),put_char('-'),write(X),barra,barra.
+        H2 =:= 3,
+        length([H1|T1],X),
+        space,write(V),put_char('-'),write(X),barra,barra.
 printcell2([[H1|T1],V,[H2|_T2]]) :- 
-	H2 =:= 4,
-	length([H1|T1],X),
-	barra,write(V),put_char('-'),write(X),barra,barra.
-	
+        H2 =:= 4,
+        length([H1|T1],X),
+        barra,write(V),put_char('-'),write(X),barra,barra.
+        
 printcell3([]).
 printcell3([[],_,_]) :- 
-	printblankcell.
+        printblankcell.
 printcell3([[_H1|_T1],_V,[H2|_T2]]) :- 
-	H2 =:= 1,
-	write('|____|').
+        H2 =:= 1,
+        write('|____|').
 printcell3([[_H1|_T1],_V,[H2|_T2]]) :- 
-	H2 =:= 2,
-	write('|   ||').
+        H2 =:= 2,
+        write('|   ||').
 printcell3([[_H1|_T1],_V,[H2|_T2]]) :- 
-	H2 =:= 3,
-	write('____||').
+        H2 =:= 3,
+        write('____||').
 printcell3([[_H1|_T1],_V,[H2|_T2]]) :- 
-	H2 =:= 4,
-	write('|___||').
+        H2 =:= 4,
+        write('|___||').
 
 printline1([]).
 printline1([ L1 | L2 ]) :- 
-	printcell1(L1),
-	printline1(L2).	
+        printcell1(L1),
+        printline1(L2). 
 printline2([]).
 printline2([ L1 | L2 ]) :- 
-	printcell2(L1),
-	printline2(L2).	
+        printcell2(L1),
+        printline2(L2). 
 printline3([]).
 printline3([ L1 | L2 ]) :- 
-	printcell3(L1),
-	printline3(L2).
-			
+        printcell3(L1),
+        printline3(L2).
+                        
 printboard([],_).
 printboard( [L1 | L2],Letter) :- 
-	length(L1,X),
-	space,space,barra,printline1(L1), nl,
-	space,put_code(Letter),barra,printline2(L1), nl,
-	space,space,barra,printline3(L1), nl,
-	space,put_code(45),barra,printboardline(X),put_code(45),nl,
-	Letter1 is Letter + 1,
-	printboard(L2, Letter1).
-	
+        length(L1,X),
+        space,space,barra,printline1(L1), nl,
+        space,put_code(Letter),barra,printline2(L1), nl,
+        space,space,barra,printline3(L1), nl,
+        space,put_code(45),barra,printboardline(X),put_code(45),nl,
+        Letter1 is Letter + 1,
+        printboard(L2, Letter1).
+        
 printpiece1([]).
 printpiece1([_Id,_V1,_V2]) :- 
-	barra,upperscore,upperscore,upperscore,upperscore,barra,
-	upperscore,upperscore,upperscore,upperscore,barra.
+        barra,upperscore,upperscore,upperscore,upperscore,barra,
+        upperscore,upperscore,upperscore,upperscore,barra.
 printpiece2([]).
 printpiece2([_Id,V1,V2]) :- 
-	write('| '),write(V1),write('  |  '),write(V2),write(' |').
+        write('| '),write(V1),write('  |  '),write(V2),write(' |').
 printpiece3([]).
 printpiece3([_Id,_V1,_V2]) :- 
-	write('|____|____|').
+        write('|____|____|').
 
 printlinepiece1([]).
 printlinepiece1([H|T]) :-
-	printpiece1(H), space,
-	printlinepiece1(T).
+        printpiece1(H), space,
+        printlinepiece1(T).
 
 printlinepiece2([]).
 printlinepiece2([H|T]) :-
-	printpiece2(H),space,
-	printlinepiece2(T).
-	
+        printpiece2(H),space,
+        printlinepiece2(T).
+        
 printlinepiece3([]).
 printlinepiece3([H|T]) :-
-	printpiece3(H),space,
-	printlinepiece3(T).
-	
+        printpiece3(H),space,
+        printlinepiece3(T).
+        
 getnmembers([],_,_,_).
 getnmembers(LI,X,LF1,LF2) :- 
-	length(LF1, X),
-	append(LF1, LF2, LI).
+        length(LF1, X),
+        append(LF1, LF2, LI).
 getnmembers(LI,_X,LI,[]).
 
 
-printplayer([]).	
+printplayer([]).        
 printplayer(L) :-
-	getnmembers(L,6,LT1,LT2),
-	printlinepiece1(LT1),nl,
-	printlinepiece2(LT1),nl,
-	printlinepiece3(LT1),nl,nl,
-	printplayer(LT2).
+        getnmembers(L,6,LT1,LT2),
+        printlinepiece1(LT1),nl,
+        printlinepiece2(LT1),nl,
+        printlinepiece3(LT1),nl,nl,
+        printplayer(LT2).
 
 printgame([]).
 printgame( [L1 | L2]) :- 
-	nl,write('DOMINUP!'),nl,
-	length(L1,X),
-	nl,space,space,space,printlinenumber(X,1),nl,
-	space,put_code(45),barra,printboardline(X),put_code(45), nl,
-	printboard([L1 | L2],65),nl.
-	
+        nl,write('DOMINUP!'),nl,
+        length(L1,X),
+        nl,space,space,space,printlinenumber(X,1),nl,
+        space,put_code(45),barra,printboardline(X),put_code(45), nl,
+        printboard([L1 | L2],65),nl.
+        
 startgame :- printgame([ [ [ [], -1 , [] ], [ [], -1 , [] ], [ [], -1 , [] ] ], [ [ [1], 1 , [2] ], [ [2], 4 , [1] ], [ [1], 5 , [3] ] ], [ [ [5], 1 , [4] ], [ [], -1 , [] ], [ [], -1 , [] ] ] ]).    
 startplayer :- printplayer([ [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0], [1,0,0] ]).
 
 /*
 printgame([ [ [ [], -1 , [] ], [ [], -1 , [] ], [ [], -1 , [] ] ], [ [ [1], 1 , [2] ], [ [2], 4 , [1] ], [ [1], 5 , [3] ] ], [ [ [5], 1 , [4] ], [ [], -1 , [] ], [ [], -1 , [] ] ] ]).    
 */
-
 
