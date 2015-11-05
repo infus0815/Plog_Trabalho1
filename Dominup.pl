d9b4faa57f3ddd1b02b9ceb210263c618a166c18
Alexandre Lima <infus@sapo.pt>
2015-11-05 03:17:29 +0000

    added some stuff

--- Dominup.pl
+++ Dominup.pl
@@ -1,55 +1,14 @@
-piece(1,0,0).
-piece(2,1,0).
-piece(3,1,1).
-piece(4,2,0).
-piece(5,2,1).
-piece(6,2,2).
-piece(7,3,0).
-piece(8,3,1).
-piece(9,3,2).
-piece(10,3,3).
-piece(11,4,0).
-piece(12,4,1).
-piece(13,4,2).
-piece(14,4,3).
-piece(15,4,4).
-piece(16,5,0).
-piece(17,5,1).
-piece(18,5,2).
-piece(19,5,3).
-piece(20,5,4).
-piece(21,5,5).
-piece(22,6,0).
-piece(23,6,1).
-piece(24,6,2).
-piece(25,6,3).
-piece(26,6,4).
-piece(27,6,5).
-piece(28,6,6).
-piece(29,7,0).
-piece(30,7,1).
-piece(31,7,2).
-piece(32,7,3).
-piece(33,7,4).
-piece(34,7,5).
-piece(35,7,6).
-piece(36,7,7).
-
-/*cada celula contem list[id.piece] valor list[orientacao] */
-board( [
-    [ [ [], -1 , [] ], [ [], -1 , [] ], [ [], -1 , [] ] ],
-    [ [ [], -1 , [] ], [ [3], 4 , [1] ], [ [], -1 , [] ] ],
-    [ [ [], -1 , [] ], [ [], -1 , [] ], [ [], -1 , [] ] ]
-   ] ). /*empty board*/
-   
-/*list de pieces*/
-p1([]). 
-p2([]).
-
-/*contem o board,pecas do p1 e p2, e o proximo jogador a jogar)*/
 
 
-game(Board-P1-P2-Nextplayer).
+startboard([ [ [ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ] ],
+             [ [ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ] ],
+             [ [ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ] ],
+             [ [ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ] ],
+             [ [ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ] ] ]).
+             
+p1([0,11,21,30,32,40,42,44,51,53,55,61,63,65,70,72,74,76]). 
+p2([10,20,22,31,33,41,43,50,52,54,60,62,64,66,71,73,75,77]).
+
 
 /*prints*/
 upperscore :- put_code(175).
@@ -153,7 +112,8 @@ printpiece1(_Id) :-
 	upperscore,upperscore,upperscore,upperscore,barra.
 printpiece2([]).
 printpiece2(Id) :- 
-	piece(Id,X,Y),
+	X is div(Id,10),
+	Y is mod(Id,10),
 	write('| '),write(X),write('  |  '),write(Y),write(' |').
 printpiece3([]).
 printpiece3(_Id) :- 
@@ -180,14 +140,30 @@ getnmembers(LI,X,LF1,LF2) :-
 	append(LF1, LF2, LI).
 getnmembers(LI,_X,LI,[]).
 
+printpiecenumber(Number):- 
+	Number < 10,
+	write('    ('),write(Number),write(')    ').
+printpiecenumber(Number):- 
+	Number >= 10,
+	write('   ('),write(Number),write(')    ').
+	
+printpiecenumberline(_,[]).
+printpiecenumberline(Number,[_H|T]):- 
+	space,space,printpiecenumber(Number),
+	NewNumber is Number + 1,
+	printpiecenumberline(NewNumber,T).
+	
 
-printplayer([]).	
-printplayer(L) :-
+printplayer([],_).	
+printplayer(L,Number) :-
 	getnmembers(L,6,LT1,LT2),
+	printpiecenumberline(Number,LT1),nl,
 	printlinepiece1(LT1),nl,
 	printlinepiece2(LT1),nl,
 	printlinepiece3(LT1),nl,nl,
-	printplayer(LT2).
+	length(LT1,X),
+	NewNumber is Number + X,
+	printplayer(LT2,NewNumber).
 
 printgame([]).
 printgame([L1 | L2]-P1-_P2-CurrentPlayer) :- 
@@ -197,17 +173,16 @@ printgame([L1 | L2]-P1-_P2-CurrentPlayer) :-
 	space,put_code(45),barra,printboardline(X),put_code(45), nl,
 	printboard([L1 | L2],65),nl,nl,
 	write('Player '),write(CurrentPlayer),write(' playing!'),nl,nl,
-	printplayer(P1).
+	printplayer(P1,1).
 	
-startgame :- printgame([ 
-	[ [ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ] ],
-	[ [ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ] ],
-	[ [ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ] ],
-	[ [ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ] ],
-	[ [ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ] ]
-	]-
-	[ 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35]-
-	1-1).    
+startgame :- 
+        p1(L1), p2(L2), 
+        startboard(B1),
+        rotatePiece(10,P),
+        putPiece(B1,1,1,P,1,Nb),
+        length(Nb,X),
+        addEmptyLine(Nb,X,B2),
+        printgame(B2-L1-L2-1). 
 
 midgame :- printgame([ 
 	[ [ [], -1 , [] ],[ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ], [ [], -1 , [] ],[ [], -1 , [] ],[ [], -1 , [] ] ],
@@ -223,5 +198,71 @@ midgame :- printgame([
 /*
 printgame([ [ [ [], -1 , [] ], [ [], -1 , [] ], [ [], -1 , [] ] ], [ [ [1], 1 , [2] ], [ [2], 4 , [1] ], [ [1], 5 , [3] ] ], [ [ [5], 1 , [4] ], [ [], -1 , [] ], [ [], -1 , [] ] ] ]).    
 */
+generateEmptyLine(L,0,L).
+generateEmptyLine(L,X,NewL) :- 
+	append(L,[[ [], -1, [] ]],NewL1),
+	X1 is X-1,
+	generateEmptyLine(NewL1,X1,NewL).
+	
+generateEmptyLine(L,X):-
+	generateEmptyLine([],X,L).
+
+addEmptyLine([H|T],X,Newboard):-
+	length(H,Size),
+	generateEmptyLine(L,Size),
+	length(H1,X),
+	append(H1,H2,[H|T]),
+	append(H1,[L|H2],Newboard).
+	
+addEmptyColumn([],_,[]).
+addEmptyColumn([H|T],X,Newboard):- 
+	length(L1,X),
+	append(L1,L2,H),
+	append( [ [] ,-1 ,[] ] ,L2,L3),
+	addEmptyColumn(T,X,Newboard2),
+	append(L1,L3,Newboard1),
+	append(Newboard1,Newboard2,Newboard).
+	
+
+rotatePiece(Piece,NewPiece):- 
+	V1 is div(Piece,10),
+	V2 is mod(Piece,10),
+	NewPiece is V2*10+V1.
+
+putPiece(Board,Line,Column,Piece,Orientation,NewBoard) :- 
+	Orientation =:= 0,
+	Line1 is Line - 1,
+	length(B1,Line1),
+	append(B1,[L|B2],Board),
+	Column1 is Column - 1,
+	length(L1,Column1),
+	append(L1,[[Id1,_,H1],[Id2,_,H2]|L2],L),
+	V1 is div(Piece,10),
+	V2 is mod(Piece,10),
+	append([Piece],Id1,NewId1),
+	append([Piece],Id2,NewId2),
+	append([1],H1,NewH1),
+	append([3],H2,NewH2),
+	append(L1,[[NewId1,V1,NewH1],[NewId2,V2,NewH2]|L2],NewL),
+	append(B1,[NewL|B2],NewBoard).
+putPiece(Board,Line,Column,Piece,Orientation,NewBoard) :- 
+	Orientation =:= 1,
+	Line1 is Line - 1,
+	length(B1,Line1),
+	append(B1,[L,M|B2],Board),
+	Column1 is Column - 1,
+	length(L1,Column1),
+	append(L1,[[Id1,_,H1]|L2],L),
+	length(L3,Column1),
+	append(L3,[[Id2,_,H2]|L4],M),
+	V1 is div(Piece,10),
+	V2 is mod(Piece,10),
+	append([Piece],Id1,NewId1),
+	append([Piece],Id2,NewId2),
+	append([2],H1,NewH1),
+	append([4],H2,NewH2),
+	append(L1,[[NewId1,V1,NewH1]|L2],NewL),
+	append(L3,[[NewId2,V2,NewH2]|L4],NewM),
+	append(B1,[NewL,NewM|B2],NewBoard).
 
 
